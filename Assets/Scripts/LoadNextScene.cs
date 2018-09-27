df8658788c76812165b69ee04961c62edd5ff566
Robin Hayes <robinh@pobox.com>
2018-09-27 15:57:41 -0700

    Added splash image and splash scene to startup sequence

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+    public float startDelay = 3f;
+    public int sceneId = 1;
+    private bool isStarted = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isStarted)
+        {
+            StartCoroutine(LoadScene());
+            isStarted = true;
+        }
+	}
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(startDelay);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneId);
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
